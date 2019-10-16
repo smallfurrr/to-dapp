381fc77d3c1684226d3396809629572c1d767270
EliseChia <swedesurrender@gmail.com>
2019-10-17 00:05:03 +0800

    i have no idea but it works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Torus from '@toruslabs/torus-embed'
+import Web3 from 'web3'
+import web3Obj from './helper'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const torus = new Torus();
+torus.init()
+
+class App extends Component {
+
+    state = {
+      account: '',
+      balance: ''
+    }
+  
+    componentDidMount() {
+      const isTorus = sessionStorage.getItem('pageUsingTorus')
+  
+      if (isTorus) {
+        web3Obj.initialize().then(() => {
+          this.setStateInfo()
+        })
+      }
+    }
+  
+    setStateInfo = () => {
+      web3Obj.web3.eth.getAccounts().then(accounts => {
+        this.setState({ account: accounts[0] })
+        web3Obj.web3.eth.getBalance(accounts[0]).then(balance => {
+          this.setState({ balance: balance })
+        })
+      })
+    }
+  
+    enableTorus = async () => {
+      try {
+        await web3Obj.initialize()
+        this.setStateInfo()
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <div>
+            <button onClick={this.enableTorus}>Start using Torus</button>
+          </div>
+          <div>
+            {/* <button onClick={this.enableTorus}>Enable Torus</button> */}
+            <div>Account: {this.state.account}</div>
+            <div>Balance: {this.state.balance}</div>
+          </div>
+        </div>
+      )
+    }
+  }
 
 export default App;
